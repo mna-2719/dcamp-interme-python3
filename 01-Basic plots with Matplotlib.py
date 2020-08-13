31a9e4b70b11b32911307db56ac09d07f303cfdb
Matt <mnabavi84@users.noreply.github.com>
2020-08-13 15:32:15 -0700

    Add files via upload

---
+++ 01-Basic plots with Matplotlib.py
@@ -0,0 +1,46 @@
+# Print the last item from year and pop
+print(year)
+print(pop)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot (year, pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap)
+print(life_exp)
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap, life_exp)
+
+# Display the plot
+plt.show()
+
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+
+# Import package
+import matplotlib.pyplot as plt
+
+# Build Scatter plot
+plt.scatter(pop, life_exp)
+
+# Show plot
+plt.show()
+
+
+
---
+++ 14-Loop Data Structures Part 1.py
@@ -0,0 +1,24 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for country, capital in europe.items():
+    print("the capital of " + str(country) + " is " + str(capital))
+
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height :
+    print(str(x) + " inches")
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print (x)
+
+
+
+    
+    
